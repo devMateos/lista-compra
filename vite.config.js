da8f3d45b2d6e8e74bad55532abcdccbaef9d4d2
antonio <antoniomateosmariscal@gmail.com>
2024-11-29 14:29:20 +0100

    Solucionando problemas con la caché

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,42 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   build: {
+    clean: true,
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
       }
     },
-    sourcemap: false
+    manifest: true,
+    assetsInlineLimit: 0,
+    publicDir: 'public',
+    base: '/'
+  },
+  server: {
+    hmr: {
+      overlay: true,
+    },
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  },
+  worker: {
+    enabled: false
   }
 })
